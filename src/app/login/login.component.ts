96356b89925ee7453691c156f95dae3f775769c5
Claudio Barra <claudiobarragomez@gmail.com>
2023-10-25 19:11:53 -0300

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userId: string = '';
+  userPassword: string = '';
 
-  constructor() { }
+  constructor(private users: UserService) { }
 
   ngOnInit() {
   }
 
+  LogIn() {
+    const userData = {
+      userId: this.userId,
+      userPassword: this.userPassword
+    };
+
+    this.users.logInUser(userData).subscribe((response) => {
+      console.log(response);
+    })
+  }
 }
