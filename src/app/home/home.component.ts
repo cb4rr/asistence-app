c882f7e10e80e9d484caf3ed808e781752997a9d
Claudio Barra <claudiobarragomez@gmail.com>
2023-10-27 13:10:54 -0300

    UI asignaturas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SubjectService } from '../services/subject.service';
 import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -11,22 +12,36 @@ import { ToastrService } from 'ngx-toastr';
 export class HomeComponent implements OnInit {
   userId: any;
   subjectTeacher: any;
+  userInfo: any;
+  todayDate: Date = new Date();
 
-  constructor(private route: ActivatedRoute, private subject: SubjectService, private toastr: ToastrService) { }
+  constructor(private route: ActivatedRoute, private subject: SubjectService, private toastr: ToastrService, private user: UserService, private router: Router) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.userId = params['teacher'];
     });
 
+    this.user.getOneUser(this.userId).subscribe({
+      next: (response) => {
+        this.userInfo = response;
+      },
+      error: () => {
+        this.toastr.error('Ha ocurrido un error al buscar al profesor.')
+      }
+    });
+
     this.subject.getAllSubjects().subscribe({
       next: (response) => {
-        this.subjectTeacher = response.data.filter((subject: any) => subject.teacherId === this.userId);
+        this.subjectTeacher = response.data.filter((subject: any) => subject.teacherId === this.userInfo.data.userId);
       },
       error: () => {
         this.toastr.error('Ha ocurrido un error al buscar las asignaturas.')
       }
-    })
+    });
   }
 
+  subjectInfo(subjectId: string) {
+    this.router.navigate(['/attendance', { teacher: this.userInfo.data._id, subjectId: subjectId }])
+  }
 }
