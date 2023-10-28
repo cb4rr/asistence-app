2ce16c01662ad8d3d34e1e77db8d74a9880142e5
Claudio Barra <claudiobarragomez@gmail.com>
2023-10-28 16:00:20 -0300

    pdf style

--- src/app/services/pdf-generator.service.ts
+++ src/app/services/pdf-generator.service.ts
@@ -12,18 +12,39 @@ export class PdfGeneratorService {
     return new Promise((resolve, reject) => {
       (pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
-      const docDefinition = {
+      const docDefinition: any = {
+        watermark: { text: 'No valido para comerse un completo', angle: 70 },
         content: [
-          { text: 'Informe de Asistencia', fontSize: 35, bold: true },
-          { text: `${studentName}`, fontSize: 24 },
-          { text: 'Detalles de asistencia:', fontSize: 17 },
-          { text: `${this.generateAttendanceDetails(attendanceData, studentIdToReport)}`, fontSize: 15 },
-          { text: 'Información adicional:', fontSize: 15 },
-          { text: `Total de sesiones: ${totalSessions}`, fontSize: 15 },
-          { text: `Asistencias: ${presentSessions}`, fontSize: 15 },
-          { text: `Porcentaje de asistencia: ${attendancePercentage}`, fontSize: 15 }
-        ]
+          { text: 'Informe de Asistencia', fontSize: 35, bold: true, margin: [0, 0, 0, 20] },
+          { text: studentName, fontSize: 24, margin: [0, 0, 0, 10] },
+          { text: 'Detalles de asistencia:', fontSize: 17, margin: [0, 20, 0, 10], bold: true },
+          { text: this.generateAttendanceDetails(attendanceData, studentIdToReport), fontSize: 15, margin: [0, 0, 0, 10] },
+          { text: 'Información adicional:', fontSize: 17, margin: [0, 20, 0, 10], bold: true },
+          { text: `Total de sesiones: ${totalSessions}`, fontSize: 15, margin: [0, 0, 0, 10] },
+          { text: `Asistencias: ${presentSessions}`, fontSize: 15, margin: [0, 0, 0, 10] },
+          { text: `Porcentaje de asistencia: ${attendancePercentage}`, fontSize: 15, margin: [0, 0, 0, 10] }
+        ],
+        styles: {
+          header: {
+            fontSize: 18,
+            bold: true,
+            alignment: 'center',
+            margin: [0, 0, 0, 20],
+          },
+          subheader: {
+            fontSize: 16,
+            bold: true,
+            margin: [0, 10, 0, 10],
+          },
+        },
       };
+
+      docDefinition.content[0].style = 'header';
+      docDefinition.content[3].style = 'subheader';
+      docDefinition.content[5].style = 'subheader';
+      docDefinition.content[6].style = 'subheader';
+      docDefinition.content[7].style = 'subheader';
+
 
       pdfMake.createPdf(docDefinition).getBlob((blob) => {
         resolve(blob);
